0b7ac8d09efabc05e776d41acf7114bfe53379db
Vrushesh Patel <vrusheshp@gmail.com>
2017-05-24 21:59:50 -0400

    push

--- 2xb3_GraphingPractice(Directed)/src/DepthFirstOrder.java
+++ 2xb3_GraphingPractice(Directed)/src/DepthFirstOrder.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class DepthFirstOrder {
 	
 	private boolean[] marked;
@@ -8,9 +10,36 @@ public class DepthFirstOrder {
 	public DepthFirstOrder(Digraph G) {
 		this.marked = new boolean[G.V()];
 		this.pre = new Queue<Integer>();
-		this.post = post;
-		this.reversePost = reversePost;
+		this.post = new Queue<Integer>();
+		this.reversePost = new Stack<Integer>();
+		
+		for (int v = 0; v < G.V(); v++){
+			if (!marked[v]) dfs(G, v);
+		}
 	}
 	
+	private void dfs(Digraph G, int v){
+		pre.enqueue(v);
+		marked[v] = true;
+		for (int w : G.adj(v)){
+			if (!marked[w]){
+				dfs(G,w);
+			}
+		}
+		post.enqueue(v);
+		reversePost.push(v);
+	}
+	
+	public Iterable<Integer> pre(){
+		return pre;
+	}
+	
+	public Iterable<Integer> post(){
+		return post;
+	}
+	
+	public Iterable<Integer> reversePost(){
+		return reversePost;
+	}
 	
 }
